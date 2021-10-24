36507d65d742de024eba7257652288c32d365bcf
magotts <techybet@gmail.com>
2021-10-24 11:40:00 -0400

    add additional links

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import React, {useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Login from "./pages/Login";
@@ -10,7 +11,7 @@ import Register from "./pages/Register";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Food_Diary from "./pages/Food_Diary";
-
+import Weight from "./pages/Weight";
 
 function App() {
 
@@ -22,16 +23,15 @@ function App() {
         <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about" exact component={About} />
+          <Route path="/weight" exact component={Weight} />
           <Route path="/food_diary" exact component={Food_Diary} />
           <Route path="/login" exact component={Login} />
           <Route path="/register" exact component={Register} />
         </Switch>
         <Footer />
       </Router>
-
     </div>
 
-    
   );
 }
 
