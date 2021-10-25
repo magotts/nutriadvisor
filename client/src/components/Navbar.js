151658f79733c6fda33925ce0826d6f62786cc50
Yirong Qin <yq4103@mun.ca>
2021-10-25 10:49:23 -0400

    add food search to nav bar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,6 +14,7 @@ export default function Navbar(props) {
       <div className="rightSide">
         <Link to="/"> Home </Link>
         <Link to="/food_diary"> Food Diary </Link>
+        <Link to="/food_search"> Food Search </Link>
         <Link to="/about"> About </Link>
         <Link to="/login"> Login </Link>
         <Link to="/register"> Register </Link>
