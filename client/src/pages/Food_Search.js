fc6d44930ccc982a00e4b13d69d541ea0e9bb687
Yirong Qin <yq4103@mun.ca>
2021-10-25 10:32:50 -0400

    remove unused imports from food search

--- client/src/pages/Food_Search.js
+++ client/src/pages/Food_Search.js
@@ -1,5 +1,4 @@
-import React, {useState, useEffect} from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
 import axios from 'axios';
 import Foods from "../components/Foods";
 import FoodAlert from "../components/FoodAlert";
