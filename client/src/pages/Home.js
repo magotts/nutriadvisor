2be108e2658a3bc1d07dd1612b0be3f5d81f766c
Yirong Qin <yq4103@mun.ca>
2021-10-26 14:46:10 -0400

    fixed home css

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,14 +1,18 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from 'axios';
 
 function Home() {
-
-
-
   return (
-    <h1>home</h1>
+
+    <div className="parallax">
+      <img class="image-1" src="https://static.wixstatic.com/media/3cd817_01efa93dfc2b4b77808f98f8915ba723~mv2.gif"></img>
+      <div className="threePics">
+      <img class="image-3" src="https://static.toiimg.com/thumb/81551320.cms?width=680&height=512&imgsize=967762"></img>
+      <img class="image-4" src="https://domf5oio6qrcr.cloudfront.net/medialibrary/2293/l0908b16207233934035.jpg"></img>
+      <img class="image-2" src="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+      </div>
+    </div>
   );
 }
-
 export default Home;
