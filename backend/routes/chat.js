c00495ca54a82630bc9701161a62a2f093cef3f1
magotts <techybet@gmail.com>
2021-10-25 20:13:48 -0400

    change port

---
+++ backend/routes/chat.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const router = express.Router();
+
+const chatRouter = (http) => {
+  const io = require("socket.io")(http, {
+    cors: {
+      origin: ["http://localhost:8000"],
+    },
+  })
+
+  io.on("connection", (socket) => {
+    // console.log(socket.id);
+    socket.on("send-message", (message) => {
+    // console.log("mesage from client", message)
+    socket.broadcast.emit("receive-message", message);
+    })
+  }) 
+return router;
+}
+
+module.exports = chatRouter;
