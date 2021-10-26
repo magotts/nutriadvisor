355dded602affbe7b5bd40cca728fb404c8581f0
magotts <techybet@gmail.com>
2021-10-25 21:23:39 -0400

    add localhost 8000

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -9,12 +9,28 @@ const chatRouter = (http) => {
   })
 
   io.on("connection", (socket) => {
-    // console.log(socket.id);
+     console.log(socket.id);
     socket.on("send-message", (message) => {
     // console.log("mesage from client", message)
     socket.broadcast.emit("receive-message", message);
     })
   }) 
+
+  // router.get("/", async (req, res) => {
+  //   console.log("user id", req.params);
+
+  //   try {
+  //   const userInfo = await db.query(`SELECT * FROM users where id = 1`);
+  //   console.log("user:", userInfo)
+  //   res.json(userInfo.rows);
+    
+  //   }
+  //   catch(err) {
+  //     console.log("error", err);
+  //     console.error(err.message)
+  //   }
+  //   });
+
 return router;
 }
 
