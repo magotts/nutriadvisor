6ca72b44bba0c096a0b334b7a8c35d856d49086f
magotts <techybet@gmail.com>
2021-10-25 22:36:38 -0400

    add chat route

--- backend/routes/chat.js
+++ backend/routes/chat.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const chatRouter = (http) => {
+const chatRouter = (http, db) => {
   const io = require("socket.io")(http, {
     cors: {
       origin: ["http://localhost:8000"],
@@ -15,21 +15,6 @@ const chatRouter = (http) => {
     socket.broadcast.emit("receive-message", message);
     })
   }) 
-
-  // router.get("/", async (req, res) => {
-  //   console.log("user id", req.params);
-
-  //   try {
-  //   const userInfo = await db.query(`SELECT * FROM users where id = 1`);
-  //   console.log("user:", userInfo)
-  //   res.json(userInfo.rows);
-    
-  //   }
-  //   catch(err) {
-  //     console.log("error", err);
-  //     console.error(err.message)
-  //   }
-  //   });
 
 return router;
 }
