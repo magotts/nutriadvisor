422004b3e3399122289b48f244850814a83604be
Yirong Qin <yq4103@mun.ca>
2021-10-27 10:50:19 -0400

    navbar changes

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -17,10 +17,11 @@ export default function Navbar(props) {
     <div className="navbar">
       <div className="leftSide">
         <img className="nav-img" src={Logo} alt="logo"/>
-        <Link to="/about"> About </Link>
+
         </div>
       <div className="rightSide">
         <Link to="/"> Home </Link>
+        <Link to="/about"> About </Link>
         <Link to="/food_diary"> Food Diary </Link>
         <Link to="/biometrics"> Biometrics </Link>
         <Link to="/food_search"> Food Search </Link>
