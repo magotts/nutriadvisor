1b29115c25ea86c0cae0c891f23bc006af6a1ee3
magotts <techybet@gmail.com>
2021-10-27 17:56:50 -0400

    add function for request a coach

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,7 @@ const biometricsRouter = require('./routes/biometrics');
 const chatRouter = require('./routes/chat');
 const userchatRouter = require('./routes/userchat');
 const userdashboardRouter = require('./routes/userdashboard');
+const requestcoachRouter = require('./routes/requestcoach');
 
 
 
@@ -35,7 +36,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-// req.session.id = "1";
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
@@ -46,20 +46,8 @@ app.use('/biometrics', biometricsRouter(db));
 app.use('/chat', chatRouter(http));
 app.use('/userchat', userchatRouter(db));
 app.use('/userdashboard', userdashboardRouter(db));
+app.use('/requestcoach', requestcoachRouter(db));
 
-
-// logout
-// app.post("/logout", (req, res) => {
-//   req.session = null;
-//   // redirect home
-//   res.redirect("/");
-// });
-
-// app.use('/login',(req, res) => {
-//   res.send({
-//     token: 'test123'
-//   });
-// })
 
 // chat app
 http.listen(5001, () => console.log("Server is running on port 5001"));
