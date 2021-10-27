011fbd16e373224457a539d152ac746ed82015e1
Smalhotra1996 <sonam.malhotra2496@gmail.com>
2021-10-26 21:44:04 -0400

    updated home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,17 +5,20 @@ import axios from 'axios';
 function Home() {
   return (
     <div class="parallax">
-
       <img class = "image-1" src ="https://static.wixstatic.com/media/3cd817_01efa93dfc2b4b77808f98f8915ba723~mv2.gif"></img>
-      <img class="image-3" src="https://www.pennmedicine.org/-/media/images/miscellaneous/fitness%20and%20sports/women_exercising.ashx"></img>
-      
+      <img class="image-3" src="https://static.toiimg.com/thumb/81551320.cms?width=680&height=512&imgsize=967762"></img>
+      <img class = "image-4" src="https://domf5oio6qrcr.cloudfront.net/medialibrary/2293/l0908b16207233934035.jpg"></img>
      
 
-     <img class="image-2" src ="https://post.healthline.com/wp-content/uploads/2019/09/granola-breakfast-fruit-healthy-1296x728-body2-1296x728.jpg"></img>
-     <img class = "image-4" src="https://domf5oio6qrcr.cloudfront.net/medialibrary/2293/l0908b16207233934035.jpg"></img>
+     <img class="image-2" src ="https://images.pexels.com/photos/1640777/pexels-photo-1640777.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"></img>
+      
+     
       
     
+    
     </div>
+
+   
   );
 }
 
