ca330d8ab0bb7973be225b01c408dd43048ebe37
magotts <techybet@gmail.com>
2021-10-29 13:30:33 -0400

    create new component for when requested coachis done

---
+++ client/src/components/CoachRequested.js
@@ -0,0 +1,13 @@
+
+const CoachRequested = (props) => {
+
+   return (
+    <div>
+      <br/>
+      <h2>Congrats! You are now connected with {props.name}!</h2>
+    </div>
+
+  )
+}
+
+export default CoachRequested;
