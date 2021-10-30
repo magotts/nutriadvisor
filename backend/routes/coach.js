429236e200a4026116bccf553695ff77eef332c8
magotts <techybet@gmail.com>
2021-10-30 00:31:05 -0400

    add route for user food diary

--- backend/routes/coach.js
+++ backend/routes/coach.js
@@ -25,9 +25,27 @@ const coachRouter = (db) => {
     }
     });
 
+     // show user food diary of user = 1 of coach id = 1
+  router.get("/userfooddiary/:id", async (req, res) => {
+
+    try {
+    const {id} = req.params;
+    console.log("coach id", req.params);
+
+    const userFoodDiary = await db.query(`select * from food_diary where user_id = $1`, [id]);
+    console.log("user fooddiary in coach profile", userFoodDiary)
+    res.json(userFoodDiary.rows);
+    
+    }
+    catch(err) {
+      console.log("error", err);
+      console.error(err.message)
+    }
+    });
 
 
   return router;
 };
 
 module.exports = coachRouter;
+
