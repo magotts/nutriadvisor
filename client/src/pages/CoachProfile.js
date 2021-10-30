820c3019a137a6fa34c1427c2a8de7091139a51c
magotts <techybet@gmail.com>
2021-10-29 23:23:11 -0400

    add some functions

--- client/src/pages/CoachProfile.js
+++ client/src/pages/CoachProfile.js
@@ -1,60 +1,38 @@
 import React, { useState, useEffect } from "react";
 import { Table } from "react-bootstrap";
 import Sidebar from "../components/Sidebar";
+import UserFoodDiary from "../components/UserFoodDiary";
 
 function CoachProfile() {
-  const [userInfo, setUserInfo] = useState([1]);
-  const [userGoal, setUserGoal] = useState([1]);
+  // const [userUnderCoach, setuserUnderCoach] = useState([1]);
+  const [userUnderCoach, setUserUnderCoach] = useState([1]);
+  const [selectedUser, setSelectedUser] = useState(null);
+  const [showFoodDiary, setShowFoodDiary] = useState(false);
 
-  const getUserInfo = async (id) => {
+
+  const getUserUnderCoach = async (id) => {
     try {
-      const response = await fetch(`http://localhost:5000/biometrics/${id}`);
+      setShowFoodDiary(false);
+      const response = await fetch(`http://localhost:5000/coach/${id}`);
       const jsonData = await response.json();
-      console.log("json", jsonData);
-      setUserInfo(jsonData);
+      console.log("coach jsondata",jsonData)
+      setUserUnderCoach(jsonData);
       return jsonData;
     } catch (err) {
       console.error(err.message);
     }
   }
 
- // show user info and their goaltype and coaches of user_id 1
-  const getGoalOfUser= async (id) => {
-    try {
-      const response = await fetch(`http://localhost:5000/biometrics/goal/${id}`);
-      const jsonData = await response.json();
-      console.log("json", jsonData)
-      setUserGoal(jsonData);
-      return jsonData;
-    }
-    catch (err) {
-      console.error(err.message)
-    }
-  }
-
-  const deleteGoal = async (id) => {
-    try {
-      const deleteGoal = await fetch(`http://localhost:5000/biometrics/goal/${id}`, {
-        method: "DELETE"
-      });
-     setUserGoal(userGoal.filter(goalUser => goalUser.id !== id));
-    }
-    catch (err) {
-      console.error(err.message)
-    }
-  }
-
   useEffect(() => {
-    getUserInfo(1);
-    console.log("info", userInfo)
-  }, []);
+    getUserUnderCoach(1);
+    console.log("USER UNDER COACH", userUnderCoach)
+  }, []); 
 
+  const usersCoach = userUnderCoach.map((users) => (
+    <h3> {users.first_name} {users.last_name} </h3>
+  ));
 
-
-  useEffect(() => {
-    getGoalOfUser(1);
-    console.log("user goal ++++++", userGoal)
-  }, []);
+  
 
   return (
     <div
@@ -68,57 +46,27 @@ function CoachProfile() {
       <Sidebar />
 
       <div className="form_center" style={{ marginLeft: "20%" }}>
-      <h1><u> User Information </u> </h1>
-      {userInfo[0] && <>
-      <span name="profile-image"><img className="img-coach" src={userInfo[0].profile_image} /> </span>
+      <h1><u> Coach Information </u> </h1>
+      {userUnderCoach[0] && <>
+      <span name="profile-image"><img className="img-coach" src={userUnderCoach[0].imageurl} /> </span>
       <br/>
           <Table striped bordered hover>
             <thead className ="table"><br/>
             <tr>
-            <th>Name:</th>
-            <td><span name="name">{userInfo[0].first_name} {userInfo[0].last_name}</span></td>
-          </tr>
-          <tr>
-            <th>Gender:</th>
-            <td><span name="gender">{userInfo[0].gender}</span></td>
-          </tr>
-          <tr>
-            <th>Age:</th>
-            <td><span name="age">{userInfo[0].age}</span></td>
-          </tr>
-          <tr>
-            <th>Height (cm):</th>
-            <td><span name="height">{userInfo[0].height}</span></td>
+            <th>Coach Name:</th>
+            <td><span name="name">{userUnderCoach[0].alias}</span></td>
           </tr>
           </thead>
-         
+          {userUnderCoach[0].first_name} {userUnderCoach[0].last_name}
          </Table>
-
-{!userGoal[0] && 
-<h3>You are currently not assigned to any coach.</h3>
-}
-
-          {userGoal[0] && <>
-            <Table striped bordered hover>
-            <thead>
-          <tr>
-            <th>Goal:</th>
-            <td><span name="goal-description">{userGoal[0].description}</span></td>
-          </tr>
-          <tr>
-            <th>Assigned Coach:</th>
-            <td><span name="coach-name">{userGoal[0].alias} </span> 
-            <button className="btn btn-danger" onClick={()=> deleteGoal(userGoal[0].id)}>Fire</button>
-            </td>
-          </tr>
-          </thead>
-         
-         </Table>
-          </>
-          }
-       
           </> 
 }
+
+
+          <h2>Users assigned to this coach:</h2>
+                {usersCoach}
+          <button onClick={()=>setShowFoodDiary(!showFoodDiary)}>Show Food Diary</button>
+          { showFoodDiary && <UserFoodDiary user={} /> } 
         </div>
         </div>
        
