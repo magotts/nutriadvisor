c18b4b40ac02c62ffab9ba86768bd62d55e0f07a
Yirong Qin <yq4103@mun.ca>
2021-11-01 15:29:42 -0400

    units for exercise

--- client/src/components/ExerciseInfo.js
+++ client/src/components/ExerciseInfo.js
@@ -7,8 +7,8 @@ const Exercises = ({ exercises }) => {
   return (
     <div className="excercisetype">
       <h4>Exercise Type and Duration: {name}</h4>
-      <h4>Exercise Duration: {duration_min}</h4>
-      <h4>Calories Burned: {nf_calories}</h4>
+      <h4>Exercise Duration: {duration_min} minutes</h4>
+      <h4>Calories Burned: {nf_calories} calories</h4>
     </div>
   );
 };
